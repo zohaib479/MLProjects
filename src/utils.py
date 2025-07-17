e591ebf3e220d104bd2e2530d8d74d99e9e3ae27
Zohaib479 <don462671@gmail.com>
2025-07-17 14:45:32 +0500

    Project Completed Not Deployment

--- src/utils.py
+++ src/utils.py
@@ -54,3 +54,10 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)  
